cf4c8f5d9ab480ee479b941be6b6a2840cfe00cb
aymanzs-3184 <asha0232@student.monash.edu>
2024-06-22 22:18:38 +1000

    Added another attribute to store the Player's name

--- player.py
+++ player.py
@@ -1,7 +1,11 @@
 class Player():
 
-    def __init__(self, playerCharacter : str) -> None:
+    def __init__(self, playerCharacter : str, playerName : str) -> None:
         self.playerCharacter = playerCharacter
+        self.playerName = playerName
 
     def getCharacter():
         return self.playerCharacter 
+    
+    def __str__(self) -> str:
+        return self.playerName
