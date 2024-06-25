c0b1faf5a3305912938c7841c40138ce0583e831
aymanzs-3184 <asha0232@student.monash.edu>
2024-06-25 17:08:15 +1000

    Added mutiple methods to the game.py and playe.pyr python files. -In the game.py file, I have added a method to return the status of the game which is represented by using an enumeration called GameStatus. -In the player.py file, I have added two methods, the getOption method gets the position in the game board chosen by the player. And the getLastChosenPosition returns the last position where the player added an X or O.

--- game.py
+++ game.py
@@ -1,10 +1,23 @@
 from player import Player
+from enum import Enum
+
 
 class AymanTicTacToe():
 
     def __init__(self) -> None:
         self.gameList = [" "," "," "," "," "," "," "," "," "]
         self.numberOfOccupiedPositions = 0
+        self.adjacentPositionsDict = {
+                                        0: [(1, 2), (3, 6), (4, 8)],
+                                        1: [(0, 2), (4, 7)],
+                                        2: [(0, 1), (5, 8), (4, 6)],
+                                        3: [(0, 6), (4, 5)],
+                                        4: [(0, 8), (2, 6), (1, 7), (3, 5)],
+                                        5: [(2, 8), (3, 4)],
+                                        6: [(0, 3), (7, 8), (2, 4)],
+                                        7: [(6, 8), (1, 4)],
+                                        8: [(0, 4), (2, 5), (6, 7)]
+                                    }
 
     def playGame(self):
         print("Welcome to Ayman's Tic Tac Toe!")
@@ -26,19 +39,21 @@ class AymanTicTacToe():
                     print("\nInvalid Option! Please Choose Another Option!")
 
     def startMultiplayerGame(self):
+        player1Character = None
         while (player1Character not in ["X", "O"]):
+
             player1Character = str(input("Enter the character for Player 1 (X or O): ")).upper()
+            self.player1 = Player(player1Character, "Player 1")
+
             match player1Character:
                 case "X":
-                    self.player1 = Player(player1Character)
                     self.player2 = Player("O")
                 case "O":
-                    self.player1 = Player(player1Character)
                     self.player2 = Player("X")
                 case _:
                     print("Invalid character! Please try entering X or O!")
 
-    def playTurn(self, player):
+    def playTurn(self, player : Player):
         pass
 
     def displayAvailablePositions(self):
@@ -48,6 +63,22 @@ class AymanTicTacToe():
                 rowNumber = (i // 3) + 1
                 printString = "Enter " + str(i + 1) + " to tick the position at Row: " + str(rowNumber) + " Column: " + str(columnNumber)
                 print(printString)
+
+    def getGameStatus(self, player : Player):
+        if self.numberOfOccupiedPositions == 9:
+            return GameStatus.DRAW
+        
+        lastOccupiedPosition = player.getLastChosenPosition()
+
+        playerCharacter = player.getCharacter()
+
+        adjacentPositions = self.adjacentPositionsDict.get(lastOccupiedPosition)
+
+        for i, j in adjacentPositions:
+            if self.gameList[i] == self.gameList[j] == playerCharacter :
+                return GameStatus.WIN
+            
+        return GameStatus.IN_PROGRESS
     
     def displayGameBoard(self):
         print("-------------")
@@ -58,3 +89,11 @@ class AymanTicTacToe():
                 characterRow += " " + self.gameList[characterPosition] + " |"
             print(characterRow)
             print("-------------")
+
+
+
+class GameStatus(Enum):
+
+    WIN = 0
+    DRAW = 1
+    IN_PROGRESS = 2
--- player.py
+++ player.py
@@ -3,9 +3,18 @@ class Player():
     def __init__(self, playerCharacter : str, playerName : str) -> None:
         self.playerCharacter = playerCharacter
         self.playerName = playerName
+        self.lastChosenPosition = None
 
     def getCharacter():
         return self.playerCharacter 
     
     def __str__(self) -> str:
         return self.playerName
+    
+    def getOption(self) -> int:
+        chosenOption = int(input("\nEnter the option here -> "))
+        self.lastChosenPosition = chosenOption
+        return chosenOption
+    
+    def getLastChosenPosition(self) -> int:
+        return self.lastChosenPosition
